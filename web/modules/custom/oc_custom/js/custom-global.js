f1ef620469c61aed58d038d42637c1a10b10fbf8
Adam Fuchs <adam@electriccitizen.com>
2018-01-12 16:18:21 -0600

    Custom warning for when no banner is used but used as back is selected

--- web/modules/custom/oc_custom/js/custom-global.js
+++ web/modules/custom/oc_custom/js/custom-global.js
@@ -2,15 +2,15 @@
 
 /* Apply select2*/
 Drupal.behaviors.mobileSelect = {
-  attach: function (context, settings) {
+	attach: function (context, settings) {
       $("select", context).once('selects').each(function(){  
-      //if (!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent))) {
-        $( 'form:not(.entity-embed-dialog) select,.sg-pattern-example select' ).select2({
-          placeholder: "Select an option"
-        });
-      //}
-    });
-  }
+	      //if (!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent))) {
+	      	$( 'form:not(.entity-embed-dialog) select,.sg-pattern-example select' ).select2({
+	      		placeholder: "Select an option"
+	      	});
+	      //}
+   	});
+   }
 };
 
 
--- web/modules/custom/oc_custom/js/custom-node.js
+++ web/modules/custom/oc_custom/js/custom-node.js
@@ -1,64 +1,59 @@
 (function($, Drupal) {
 
 Drupal.behaviors.admin_bkimage = {
-  attach: function (context, settings) {
-
-  	 $(".field--name-field-oc-widgets", context).once('style').each(function(){  
-  	 		$(document).ajaxComplete(function() {
-  	 			$('.field--name-field-background-style').each(function(){
-  	 				var styler = $(this);
-  	 				var chosen = $('select', this).find("option:selected").val()
-  	 				if(chosen != 'image'){
-  	 					styler.next('.field--name-field-background-image').hide();
-  	 				}else{
-  	 					styler.next('.field--name-field-background-image').show();
-  	 				}
-  	 				$(this).find('select').change(function(){
-  	 					//alert('changed');
-  	 					var choice = $(this).find("option:selected").val()
-  	 					if(choice == 'image'){
-  	 						styler.next('.field--name-field-background-image').show();
-  	 					}else{
-  	 						styler.next('.field--name-field-background-image').hide();
-  	 					}
-  	 				});
-  	 			});
-  	 		});
-
-        //send help when multiple paragraphs are on a page
-        $(document).ajaxComplete(function(){
-            var count = $('.paragraphs-preview').length;
-            if(count > 5){
-              if(!$('.reminder.collapsy').length){
-                $('body').append('<div class="reminder collapsy"><span class="close"></span>When using multiple paragraphs with layout options to create your page, its helpful to use the \'Preview\' mode to view the page.</div>');
-                $('.collapsy').delay(1000).animate({right: "+=400px"}, 500, "linear");
-                //$('.collapsy').delay(7000).animate({right: "-=400px"}, 500, "linear");
-                $('.collapsy').click(function(){
-                  $(this).stop(true,true).fadeOut(200);
-                });
-              }
-            }
-        });
-	 });
-  }
+	attach: function (context, settings) {
+		$(".field--name-field-oc-widgets", context).once('style').each(function(){  
+			$(document).ajaxComplete(function() {
+				$('.field--name-field-background-style').each(function(){
+					var styler = $(this);
+					var chosen = $('select', this).find("option:selected").val();
+					if(chosen != 'image'){
+						styler.next('.field--name-field-background-image').hide();
+					}else{
+						styler.next('.field--name-field-background-image').show();
+					}
+					$(this).find('select').change(function(){
+						//alert('changed');
+						var choice = $(this).find("option:selected").val();
+						if(choice == 'image'){
+							styler.next('.field--name-field-background-image').show();
+						}else{
+							styler.next('.field--name-field-background-image').hide();
+						}
+					});
+				});
+			});
+		});
+	}
 };
 
-// Drupal.behaviors.scrollOpen = {
-//   attach: function (context, settings) {
+Drupal.behaviors.admin_bannerBack = {
+	attach: function (context, settings) {
+		$(".field--name-field-use-as-background", context).once('hasImage').each(function(){  
 
-//     $(".field--name-field-widgets", context).once('scrollOpen').each(function(){  
-//       $('.paragraphs-dropbutton-wrapper input.field-add-more-submit').mousedown(function(){
-//           $(document).ajaxComplete(function(){
-//             if(!$(this).parent('.ajax-new-content').length){
-//               $('html, body').animate({
-//                 scrollTop: $(".field--name-field-widgets .draggable:last").offset().top - 100
-//               }, 'slow');
-//             }
-//           });
-//       });
-//    });
-//   }
-// };
+			$('.field--name-field-use-as-background').each(function(){
+				if(($('input:checked', this).length) && (!$('#ief-entity-table-edit-field-banner-entities').length)){
+					$(this).prepend('<div class="custom-warning">Make sure you add a banner to use as the background.</div>');
+					setTimeout(function() {
+						$('.custom-warning').remove();
+					}, 6000);
+				}
+				$('input', this).change(function(){
+					$('.custom-warning').remove();
+					if((this.checked) && (!$('#ief-entity-table-edit-field-banner-entities').length)){
+						$('.field--name-field-use-as-background').prepend('<div class="custom-warning">Make sure you add a banner to use as the background.</div>');
+						setTimeout(function() {
+							$('.custom-warning').remove();
+						}, 3000);
+					}
+				});
+			});
+		
+		});
+	}
+};
+
+
 
 
 })(jQuery, Drupal);
